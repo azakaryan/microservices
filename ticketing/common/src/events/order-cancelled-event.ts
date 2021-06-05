3b0c0d317d9733e444a7e2e17d3217a1dceffcbd
azakaryan <armenzakary@gmail.com>
2021-06-05 17:20:18 +0200

    Updates

--- ticketing/common/src/events/order-cancelled-event.ts
+++ ticketing/common/src/events/order-cancelled-event.ts
@@ -1,16 +1,11 @@
 import { Subjects } from './subjects';
-import { OrderStatus } from './types/order-status';
 
 export interface OrderCancelledEvent {
-  subject: Subjects.OrderCreated;
+  subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    userId: string;
-    status: OrderStatus;
-    expiresAt: string;
     ticket: {
       id: string;
-      price: number;
     }
   };
 }
